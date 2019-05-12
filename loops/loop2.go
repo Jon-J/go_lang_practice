c9b846cbfc0004bd25fd8ad06e20dcf33eecb4e1
Jon-J <bv.sheetalkumar@gmail.com>
2019-05-12 21:09:50 +0530

    update

---
+++ loops/loop2.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main(){
+	for a := 0;a < 3; a++{
+		for b := 3;b > 0; b--{
+			fmt.Printf("a - %d, b - %d\n", a, b)
+		}
+	}
+}
---
+++ practice/test.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+
+	var sid int
+	sid = 10
+	if (sid%2 == 0){
+		fmt.Printf("Hi sid - %d\n", sid)
+	}
+}
